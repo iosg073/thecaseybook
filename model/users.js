6ac8279c982373e984dd8e4654b4d245025c85e5
alam <walam@up.com>
2021-05-14 11:32:15 -0500

    modle is added

---
+++ model/users.js
@@ -0,0 +1,11 @@
+const users =[
+    {
+        username: " CRHarding",
+        email: "c@c",
+        password: "casey123",
+        profile: "Lorem ipsum dolor ",
+        img: "https://encrypted-tbn0.gstatic.com/shopping?q=tbn:ANd9GcSfTaiiDLvoYd0RwYUQjpRmb4betv1_78W6MKKeel4R4ahYL6i2h6jy4kD3hBUp3BOHfcpqj8-e6W59&usqp=CAc ",
+        uuid:12345
+    }
+]
+module.exports=users;
